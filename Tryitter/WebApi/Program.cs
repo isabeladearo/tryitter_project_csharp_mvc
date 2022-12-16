83474201e12ddb48b95dbfc680371e0a3788069a
Isabela de Aro <euisabeladearo@gmail.com>
2022-12-15 21:09:04 -0300

    remove service reference

--- Tryitter/WebApi/Program.cs
+++ Tryitter/WebApi/Program.cs
@@ -124,6 +124,4 @@ void ConfigureServices(WebApplicationBuilder builder)
     builder.Services.AddScoped<IStudentRepository, StudentRepository>();
     builder.Services.AddScoped<IPostRepository, PostRepository>();
     builder.Services.AddTransient<ITokenService, TokenService>();
-    builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-    builder.Services.AddScoped<AuthenticateUserService>();
 }
